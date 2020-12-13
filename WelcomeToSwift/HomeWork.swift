c9386c966dc31d6538f0e2d88d7632a9e5c604ae
Dauken <pro100_gold_man@mail.ru>
2020-12-13 15:19:47 +0600

    homework lesson 1

---
+++ WelcomeToSwift/HomeWork.swift
@@ -0,0 +1,8 @@
+//
+//  HomeWork.swift
+//  WelcomeToSwift
+//
+//  Created by Ниязов Ринат Раимжанович on 12/13/20.
+//
+
+import Foundation
--- WelcomeToSwift/main.swift
+++ WelcomeToSwift/main.swift
@@ -8,48 +8,50 @@
 import Foundation
 
 
-let employList: [String] = [
-    "Геннадий Новиков",
-    "Давид С",
-    "Давид С",
-]
+// Task 1 square
+let A_value = 2
+let B_value = 4
+let C_value = 1
 
-print(employList)
+var discriminant = B_value * B_value - 4 * A_value * C_value
 
-let employListSet: Set<String> = [
-    "Геннадий Новиков",
-    "Давид С",
-    "Давид С"
-]
-
-print(employListSet)
-
-
-let isSergeyInList = employList.contains("Давид С")
-
-print(isSergeyInList)
-
-
-
-var r: Double? = .none //server закончил работу с ошибкой
-
-
-
-if let value = r {
-    print(value)
-} else {
-    print("Server ничего не вернул")
+if(discriminant < 0) {
+    print("Так как дискриминант меньше нуля, то уравнение не имеет действительных решений.")
 }
-
-let result = r ?? 0
-
-
-switch r {
-case .none: print("Server ничего не вернул")
-case let .some(value): print(value)
+else {
+    print("Так как дискриминант больше нуля то, квадратное уравнение имеет два действительных корня:")
+    
+    let sqrtDiscriminant = sqrt(Double(discriminant))
+    let X1_result = (Double(-B_value) - sqrtDiscriminant) / Double(A_value * A_value)
+    let X2_result = (Double(-B_value) + sqrtDiscriminant) / Double(A_value * A_value)
+    
+    print ("D = ",discriminant, "\nx1 = ", X1_result, "\nx2 = ", X2_result)
 }
 
 
+// Task 2
+let a_cathetus: Double = 5
+let b_cathetus: Double = 4
+
+let c_hypotenuse: Double = sqrt(a_cathetus * a_cathetus + b_cathetus * b_cathetus)
+let perimeter: Double = c_hypotenuse + a_cathetus + b_cathetus
+let S_area: Double = a_cathetus * b_cathetus / 2
+
+print("S = ", S_area, "P = ", perimeter, "Гипотенуза = ", c_hypotenuse)
 
 
+// Task 3
+print("Введите сумму вклада:")
+let depositInput = Double(readLine()!)
+print("Введите годовой процент:")
+let percentInput = Double(readLine()!)
 
+if(depositInput != nil && percentInput != nil){
+    var finalDeposit = Double(depositInput ?? 0.0);
+    for _ in 1...5 {
+        finalDeposit += (finalDeposit * (percentInput ?? 0.0) / 100)
+    }
+
+    print("Итоговое вознаграждение за 5 лет: \(finalDeposit)")
+
+}
